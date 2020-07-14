c431585bc1775b23038ff1441d94833f19589dec
Jason Grout <grout58@gmail.com>
2020-07-14 16:48:26 -0400

    Update sw.py

--- sw.py
+++ sw.py
@@ -2,13 +2,15 @@ import requests
 import json
 
 
-cities = [
-    {'name': 'Boston, MA', 'lat': '42.3601', 'lon': '-71.0589'},
-    {'name': 'North Andover, MA', 'lat': '42.671970', 'lon': '-71.087440'},
-    {'name': 'New York', 'lat': '40.7128', 'lon': '74.0060'},
-    {'name': 'St. Johnsbury, VT', 'lat': '44.4193', 'lon': '-72.0151'},
-    {'name': 'North Adams, MA', 'lat': '42.7009', 'lon': '-73.1087'}
-]
+# Place all city names in text file names cities.txt
+file = open('cities.txt', 'r')
+
+data = file.readlines()
+cities = []
+high_temp_list = []
+
+for i in data:
+    cities.append(i.strip())
 
 
 # converts from kelvin to fahrenheit and returns rounded number
@@ -17,8 +19,22 @@ def convert_temp(temp):
     return ftemp
 
 
-def get_weather(lat, lon, name):
-    key = 'API-KEY'
+def get_coords(city_name):
+    key = 'API_KEY'
+    api_call = f'https://api.opencagedata.com/geocode/v1/json?q={city_name}&key={key}'
+
+    response = requests.get(api_call)
+    location_data = json.loads(response.text)
+
+    city_name = location_data['results'][0]['formatted']
+    lat = location_data['results'][0]['bounds']['northeast']['lat']
+    lon = location_data['results'][0]['bounds']['northeast']['lng']
+
+    return{'lat': lat, 'lon': lon, 'city_name': city_name}
+
+
+def get_weather(lat, lon, city_name):
+    key = 'API_KEY'
     apiCall = f'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={key}'
     response = requests.get(apiCall)
     weatherDataAll = json.loads(response.text)
@@ -27,15 +43,27 @@ def get_weather(lat, lon, name):
     highTemp = convert_temp(tempData['max'])
     lowTemp = convert_temp(tempData['min'])
 
-    return {'humidity': humidityData, "highTemp": highTemp, "lowTemp": lowTemp, "name": name}
+    return {'humidity': humidityData, "highTemp": highTemp, "lowTemp": lowTemp, "city_name": city_name}
 
 
 for city in cities:
-    get_weather_data = get_weather(city['lat'], city['lon'], city['name'])
-
+    city_data = get_coords(city)
+    get_weather_data = get_weather(city_data['lat'], city_data['lon'], city_data['city_name'])
     highTemp = get_weather_data['highTemp']
     lowTemp = get_weather_data['lowTemp']
     humidityData = get_weather_data['humidity']
-    name = get_weather_data['name']
+    city_name = get_weather_data['city_name']
 
-    print(f' Name: {name} \n Low: {lowTemp} \n High: {highTemp} \n Humidity: {humidityData} \n')
+    print(f' Name: {city_name} \n Low: {lowTemp} \n High: {highTemp} \n Humidity: {humidityData} \n')
+
+    high_temp_list.append((city_name, highTemp))
+
+high_temp_list.sort(key=lambda x: x[1])
+
+
+hottest_city, hottest_temp = high_temp_list[-1]
+coolest_city, coolest_temp = high_temp_list[0]
+
+print(f' The hottest city in the US is {hottest_city} at {hottest_temp}')
+print(f' The coolest city in the US is {coolest_city} at {coolest_temp}')
+
